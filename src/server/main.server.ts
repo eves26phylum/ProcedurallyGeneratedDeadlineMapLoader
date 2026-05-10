72834fc3c7488bed131f7dd2472b28d1e905d2df
eves <275312891+eves26phylum@users.noreply.github.com>
2026-05-10 22:14:43 +1000

    i think i should add an npm repo for deadline-ts and name it dog, so you can do dog, build

---
+++ src/server/main.server.ts
@@ -0,0 +1,3 @@
+// !deadline-ts.customFinishSector_FinishModulesEnd
+set_require_domain("https://raw.githubusercontent.com/eves26phylum/ProcedurallyGeneratedDeadlineMap_TS/main/deadlineout/");
+require("server.luau");
